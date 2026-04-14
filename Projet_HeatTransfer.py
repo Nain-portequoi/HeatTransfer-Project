ca3f38c7ceaba181f946000884a7df3c1a366d4c
Tom Kalscheuer <kom@Toms-MacBook-Air.local>
2026-04-14 15:03:33 +0200

    Premier commit

---
+++ Projet_HeatTransfer.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as pltt
+
+#Dimension de la surface
+eM = 20 # cm
+eI = 12
+eS = 3
+eB = 5
+lM = 300
+hM = 200
+hB = 10
+k = 1,5
+
+#Pas
+p = 20
+
+print("DADAA")
